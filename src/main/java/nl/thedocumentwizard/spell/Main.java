5ceeaf5872a467dd242c6b25cac4870dd736315d
Jose Ramon Cano Yribarren <jose.cano@theconsultancyfirm.com>
2016-12-04 19:09:22 +0100

    Add a Main class as the entry point of the JAR.

---
+++ src/main/java/nl/thedocumentwizard/spell/Main.java
@@ -0,0 +1,10 @@
+package nl.thedocumentwizard.spell;
+
+/**
+ * Created by jose on 04/12/2016.
+ */
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Hello world");
+    }
+}
