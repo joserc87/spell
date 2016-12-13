410daaf6eac463a721351009f468f95e21cdfc25
Jose Ramon Cano Yribarren <jose.cano@theconsultancyfirm.com>
2016-12-13 16:24:36 +0100

    Add document types XSD

--- xjc.sh
+++ xjc.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 # Generates the Java classes from the XSDs
 xjc src/main/resources/WizardConfiguration.xsd -d src/main/java -p nl.thedocumentwizard.wizardconfiguration.jaxb
+xjc src/main/resources/documenttype.1.0.xsd -d src/main/java #-p nl.thedocumentwizard.wizardconfiguration.jaxb
