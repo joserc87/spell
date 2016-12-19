992f8624ff3cb84c81f8e4d2cc354286afad8837
Jose Ramon Cano Yribarren <jose.cano@theconsultancyfirm.com>
2016-12-19 15:17:44 +0100

    Add a helper class

---
+++ src/main/java/nl/thedocumentwizard/spell/parser/ParsingHelper.java
@@ -0,0 +1,72 @@
+package nl.thedocumentwizard.spell.parser;
+
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * Created by jose on 19/12/2016.
+ */
+public class ParsingHelper {
+
+    /**
+     * Parses a string with single or double quotes and returns the content
+     * 
+     * @param quotedString A string with the format "content"
+     *
+     * @return A string with the content
+     */
+    public String getString(String quotedString) {
+        assert (quotedString != null && quotedString.length() >= 2 &&
+                quotedString.charAt(0) == quotedString.charAt(quotedString.length() - 1) &&
+                (quotedString.charAt(0) == '"' || quotedString.charAt(0) == '\''));
+        return quotedString.substring(1, quotedString.length() - 1);
+
+    }
+
+    /**
+     * Accepts a TerminalNode of type STRING as a parameter and returns the
+     * content of the string, without quotes
+     * 
+     * @param node The terminal node
+     *
+     * @return A string with the content
+     */
+    public String getString(TerminalNode node) {
+        if (node != null && node.getText() != null) {
+            return this.getString(node.getText());
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Parses a metadata string with a $ and returns the metadata name without
+     * the $
+     * 
+     * @param $metadataName A string with the metadata name, including the
+     * dollar sign
+     *
+     * @return A string with the metadata name, excluding the first position
+     * ($).
+     */
+    public String getMetadataName(String $metadataName) {
+        assert ($metadataName != null && $metadataName.length() >= 1 && $metadataName.charAt(0) == '$');
+        return $metadataName.substring(1);
+    }
+
+    /**
+     * Accepts a TerminalNode of type METADATA as a parameter and returns the
+     * content of the metadata name, without the preceding dollar sign
+     * 
+     * @param node The terminal node
+     *
+     * @return A string with the metadata name
+     */
+    public String getMetadataName(TerminalNode node) {
+        if (node != null) {
+            String $metadataName = node.getText();
+            return this.getMetadataName($metadataName);
+        } else {
+            return null;
+        }
+    }
+}
---
+++ src/test/java/nl/thedocumentwizard/spell/parser/ParsingHelperTest.java
@@ -0,0 +1,70 @@
+package nl.thedocumentwizard.spell.parser;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.antlr.v4.runtime.tree.TerminalNode;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * Test the helper
+ */
+public class ParsingHelperTest {
+
+    ParsingHelper helper; // SUT
+    @Before
+    public void becauseThereIsAHelper() {
+        helper = new ParsingHelper();
+    }
+
+    @Test
+    public void should_get_content_of_string_when_correct() {
+        Assert.assertEquals("", helper.getString("\'\'"));
+        Assert.assertEquals("a", helper.getString("\'a\'"));
+        Assert.assertEquals("asdf", helper.getString("\"asdf\""));
+        Assert.assertEquals(" text \n with \t multiple \n lines ", helper.getString("\' text \n with \t multiple \n lines \'"));
+    }
+
+    @Test
+    public void should_get_content_of_string_node_when_correct() {
+        TerminalNode node = mock(TerminalNode.class);
+
+        when(node.getText()).thenReturn("\'\'");
+        Assert.assertEquals("", helper.getString(node));
+
+        when(node.getText()).thenReturn("\'a\'");
+        Assert.assertEquals("a", helper.getString(node));
+
+        when(node.getText()).thenReturn("\"asdf\"");
+        Assert.assertEquals("asdf", helper.getString(node));
+
+        when(node.getText()).thenReturn("\' text \n with \t multiple \n lines \'");
+        Assert.assertEquals(" text \n with \t multiple \n lines ", helper.getString(node));
+    }
+
+    @Test
+    public void should_get_content_of_metadata_string_when_correct() {
+        Assert.assertEquals("", helper.getMetadataName("$"));
+        Assert.assertEquals("metadata1", helper.getMetadataName("$metadata1"));
+        Assert.assertEquals("$metadata1", helper.getMetadataName("$$metadata1"));
+    }
+
+    @Test
+    public void should_get_content_of_metadata_node_when_correct() {
+        TerminalNode node = mock(TerminalNode.class);
+
+        when(node.getText()).thenReturn("$");
+        Assert.assertEquals("", helper.getMetadataName(node));
+
+        when(node.getText()).thenReturn("$metadata1");
+        Assert.assertEquals("metadata1", helper.getMetadataName(node));
+
+        when(node.getText()).thenReturn("$$metadata1");
+        Assert.assertEquals("$metadata1", helper.getMetadataName(node));
+    }
+}
